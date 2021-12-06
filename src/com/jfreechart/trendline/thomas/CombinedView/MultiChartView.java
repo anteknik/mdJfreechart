a730ef0a2534700c3dcc96b230853412a6ca0d49
Anton purwanto <anton.takaful@gmail.com>
2021-12-06 22:08:06 +0700

    implementing stocksplit provider

--- src/com/jfreechart/trendline/thomas/CombinedView/MultiChartView.java
+++ src/com/jfreechart/trendline/thomas/CombinedView/MultiChartView.java
@@ -1,14 +1,21 @@
 /**
- * 
+ *
  */
 package com.jfreechart.trendline.thomas.CombinedView;
 
+import static com.jfreechart.trendline.thomas.CombinedView.StockSplitResourcesProvider.SELECTOR;
+import static com.jfreechart.trendline.thomas.CombinedView.StockSplitResourcesProvider.URL;
+import static com.jfreechart.trendline.thomas.CombinedView.StockSplitResourcesProvider.getListStockSplits;
+import com.jfreechart.trendline.thomas.OHLCCandleStickTrendLineChart;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,96 +40,113 @@ import yahoofinance.histquotes.Interval;
  * @author Ghanshyam Vaghasiya
  *
  */
-public class MultiChartView extends Application{
+public class MultiChartView extends Application {
 
-	/**
-	 * @param args
-	 */
-	
-	private static Stage topPrimaryStage; 
-	private static Logger log = LoggerFactory.getLogger(MultiChartView.class);
-	private static ArrayList stockName = new ArrayList();
+    /**
+     * @param args
+     */
+    private static Stage topPrimaryStage;
+    private static Logger log = LoggerFactory.getLogger(MultiChartView.class);
+    private static ArrayList stockName = new ArrayList();
 
+    public static void main(String args[]) {
+        LoggerManager.initializeLoggingContext();
+        log.info("Init Start");
+        launch(args);
 
-	public static void main(String args[]){
-		LoggerManager.initializeLoggingContext();
-		log.info("Init Start");
-		launch(args);
+    }
 
-	}
-	
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        getContractList();
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		 	getContractList();
-	        
-	        topPrimaryStage = primaryStage;
-	        FlowPane root = new FlowPane();
-	        root.setAlignment(Pos.CENTER);
-	        Scene scene = getMainScene();
-	        
-			primaryStage.setScene(scene);
-			primaryStage.setTitle("MultiChart View");
-			primaryStage.show();
-			
-	}
-	
-	@SuppressWarnings({ "unchecked", "unused", "rawtypes" })
-	private Scene getMainScene() {
-	    Label symbolNameLabel = new Label("Symbol: ");
-		ComboBox symbolcombo_box = new ComboBox(FXCollections.observableArrayList(stockName));	
-		
-		symbolcombo_box.valueProperty().addListener(new ChangeListener<String>() {
-			@Override
-			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-			if(stockName.contains(newValue)){
-				symbolcombo_box.setValue(newValue);
-			}		
+        topPrimaryStage = primaryStage;
+        FlowPane root = new FlowPane();
+        root.setAlignment(Pos.CENTER);
+        Scene scene = getMainScene();
 
-			}
-		});
-		
-		new AutoCompleteComboBox.AutoCompleteComboBoxListener<>(symbolcombo_box);
-		
-		Button btn1=new Button("Draw Chart");
-		btn1.setStyle("-fx-background-color: lightblue;; -fx-text-fill:darkblue; ");
-		btn1.setMaxWidth(100);
-		btn1.setOnAction(new EventHandler<ActionEvent>(){
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("MultiChart View");
+        primaryStage.show();
 
-			@Override
-			public void handle(ActionEvent event) {
+    }
 
-				try {
-					                               System.out.println("click get value : "+(String)symbolcombo_box.getValue());
-					new OHLCMultiSymbolTrendLineChart((String)symbolcombo_box.getValue(),Calendar.YEAR, -10,Interval.DAILY);
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-			}
-			
-		});
+    @SuppressWarnings({"unchecked", "unused", "rawtypes"})
+    private Scene getMainScene() {
 
-	
-		FlowPane root = new FlowPane(symbolNameLabel,symbolcombo_box,btn1);
+        // params to get getListStockSplits
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("EEE MMM dd, yyyy");
+        LocalDate localDate = LocalDate.now();
+        //LocalDate localDate = date.minusDays(1); //backdate
+        String urlparam = URL + "day=" + localDate + "&offset=0&sf=startdatetime&st=asc";
+         //
+
+        Label symbolNameLabel = new Label("Symbol: ");
+        ComboBox symbolcombo_box = new ComboBox(FXCollections.observableArrayList(stockName));
+
+        symbolcombo_box.valueProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if (stockName.contains(newValue)) {
+                    symbolcombo_box.setValue(newValue);
+                }
+
+            }
+        });
+
+        new AutoCompleteComboBox.AutoCompleteComboBoxListener<>(symbolcombo_box);
+
+        Button btn1 = new Button("Draw Chart");
+        btn1.setStyle("-fx-background-color: lightblue;; -fx-text-fill:darkblue; ");
+        btn1.setMaxWidth(100);
+        btn1.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+
+                try {
+                    List<String> listSymbol = getListStockSplits(localDate, urlparam, SELECTOR);
+                    String symbolcombo = (String) symbolcombo_box.getValue();
+                    for (String symbol : listSymbol) {
+                        if (symbolcombo.equalsIgnoreCase(symbol)) {
+                            log.info(symbolcombo+" == "+ symbol);
+                            new OHLCCandleStickTrendLineChart(symbol, Calendar.YEAR, -1, Interval.DAILY);
+                            break;
+                        } else {
+
+                            log.info(symbolcombo+" != "+ symbol);
+                            new OHLCMultiSymbolTrendLineChart((String) symbolcombo_box.getValue(), Calendar.YEAR, -10, Interval.DAILY);
+                        }
+                        
+                    }
+
+                } catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+            }
+
+        });
+
+        FlowPane root = new FlowPane(symbolNameLabel, symbolcombo_box, btn1);
         root.setAlignment(Pos.CENTER);
         Scene scene = new Scene(root, 400, 100);
-		return scene;
-	}
-	
-	@SuppressWarnings("unchecked")
-	public static void getContractList() throws IOException{
-		File file = new File("conf\\nasdaq_screener_1630576232472.csv");
-		FileReader fr = new FileReader(file); 
-		BufferedReader br = new BufferedReader(fr);
-		String line ;
-		while((line = br.readLine())!=null ){
-				String tokens[] = line.split(",");
-				String symbolName = tokens[0].replace(" ", "");
-				stockName.add(symbolName);
-		} 
-		
-	}
+        return scene;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static void getContractList() throws IOException {
+        File file = new File("conf\\nasdaq_screener_1630576232472.csv");
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
+        String line;
+        while ((line = br.readLine()) != null) {
+            String tokens[] = line.split(",");
+            String symbolName = tokens[0].replace(" ", "");
+            stockName.add(symbolName);
+        }
+
+    }
 
 }
